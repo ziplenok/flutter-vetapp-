b2b34a9167461841f427b7c30802bb95dfd589a4
ziplenok <shivczov00@gmail.com>
2021-10-19 16:15:33 +0600

    updated project with main screen

---
+++ vet_project/lib/app_constants.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: use_full_hex_values_for_flutter_colors
+
+import 'package:flutter/material.dart';
+
+
+// Константы для приложения 
+
+class AppColors {
+
+  static const Color PRIMARY_COLOR = Color(0xffe5e5e5);
+  static const Color ACCENT_COLOR = Color(0xffff7800);
+  static const Color GREY_COLOR = Color(0xff212529);
+}
